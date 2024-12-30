41942670c565630be397ccaa40c1a64357bd7dc6
wjinss <aastu4343@gmail.com>
2024-12-31 04:28:08 +0900

    에어비앤비 경로 수정

--- src/util/getPublishungContents.js
+++ src/util/getPublishungContents.js
@@ -42,8 +42,7 @@ const getPublishungContents = [
     period: "기간: 디자인 7일, 코딩 7일",
     part: "기여도: 디자인, 코딩 33%",
     tools: [1, 2, 4, 6],
-    href: "http://aastu4343.dothome.co.kr/web_contents/airnbnb/index.html",
-    // onclick="window.open(this.href, '_blank', 'width=400, height=1000"
+    href: "http://aastu4343.dothome.co.kr/web_contents/airbnb/index.html",
   },
   {
     id: 6,

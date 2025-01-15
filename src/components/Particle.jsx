83b6a92af65a686eb303b44bd28390075af375fc
wjinss <aastu4343@gmail.com>
2025-01-15 16:50:31 +0900

    파티클 위치 조정

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
 
--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -42,24 +42,26 @@ const Visual = () => {
     });
   }, []);
   return (
-    <section id="Visual" className="sec">
+    <>
       <Particle />
-      <div className="in sec_in">
-        <div className="visual_tit_wrap">
-          <h2 className="visual_tit p_bold">안녕하세요!</h2>
-          <p className="visual_txt p_bold">
-            어제보다 오늘 더 발전하는 _<br /> 웹 퍼블리셔 정우진입니다
-          </p>
+      <section id="Visual" className="sec">
+        <div className="in sec_in">
+          <div className="visual_tit_wrap">
+            <h2 className="visual_tit p_bold">안녕하세요!</h2>
+            <p className="visual_txt p_bold">
+              어제보다 오늘 더 발전하는 _<br /> 웹 퍼블리셔 정우진입니다
+            </p>
+          </div>
+          <div className="visual_img">
+            <img src={visualImg} alt="비주얼 이미지" />
+          </div>
+          <div className="scroll">
+            <img src={scrollIcon} alt="스크롤 해주세요" />
+            <p>Scroll</p>
+          </div>
         </div>
-        <div className="visual_img">
-          <img src={visualImg} alt="비주얼 이미지" />
-        </div>
-        <div className="scroll">
-          <img src={scrollIcon} alt="스크롤 해주세요" />
-          <p>Scroll</p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default Visual;
